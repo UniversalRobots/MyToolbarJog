e7e6a05daabea08e7e95927c48176a22203562d5
Nicolai Anton Lynnerup <naly@universal-robots.com>
2022-11-09 11:34:09 +0100

    Removed checks on SDK version and removed sudo errors from install script

--- .github/actions/build_urcap/entrypoint.sh
+++ .github/actions/build_urcap/entrypoint.sh
@@ -2,20 +2,16 @@
 
 set -e
 
-SDK_MAJOR=$(echo ${SDK_VERSION} | awk -F. '{print $1}')
-SDK_MINOR=$(echo ${SDK_VERSION} | awk -F. '{print $2}')
-SDK_BUILD=$(echo ${SDK_VERSION} | awk -F. '{print $3}')
-
 download_folder=$(mktemp -d)
 pushd ${download_folder}
 curl -o sdk-${SDK_VERSION}.zip https://s3-eu-west-1.amazonaws.com/urplus-developer-site/sdk/sdk-${SDK_VERSION}.zip
 mkdir sdk
 unzip -q sdk-${SDK_VERSION}.zip -d sdk
 cd sdk
-./install.sh
-
+sed -e 's/\<sudo\>//g' ./install.sh > ./install_no_sudo.sh
+chmod +x ./install_no_sudo.sh
+yes | ./install_no_sudo.sh
 popd
 
 cd com.ur.urcap.examples.mytoolbarjog/
-echo $PWD
 mvn install
